94d223fdc324ce08e49c3517372e7bd87845f30c
Monday David Osakpawan <rynstheoverlord@gmail.com>
2025-10-15 04:15:35 +0100

    Reformated file and added support for firefox

--- main.py
+++ main.py
@@ -4,38 +4,52 @@ from selenium.webdriver.firefox.options import Options as FireFoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import click
-import time, sys  
+import time, questionary
 
 URL = "https://moviebox.ph/"
-chrome_options = ChromeOptions()
+driver_options = None
+
+@click.group(help="Download movies from Moviebox to your device")
+def cli():
+    pass 
 
 def handle_headless(is_headless: bool): 
+    if driver_options is None:
+        click.echo(click.style("Driver not set!", fg="red"))
+        return
     click.echo(f"headless: {is_headless}")
     if is_headless:
-        chrome_options.add_argument("--headless")
+        driver_options.add_argument("--headless")
 
-@click.command()
+
+
+
+@cli.command(help="Find movie to download")
 @click.option("-s", "--search", default="", help="Show to search for")
 @click.option("--browser-id", default=0, help="Set browser by ID, 0 for chrome 1 for firefox")
 @click.option("-t", "--timeout", default=3, help="Set length of wait intervals")
 @click.option("-h", "--headless", is_flag=True, help="Use to hide the browser")
-def visit_site(search, browser_id, timeout, headless):
+def find(search, browser_id, timeout, headless):
+    global driver_options
     driver = None
     match browser_id:
         case 0:
-            click.echo("Using Chrome...")
+            click.echo("Driver: Chrome")
+            driver_options = ChromeOptions()
             handle_headless(headless)
-            driver = webdriver.Chrome(options=chrome_options)
+            driver = webdriver.Chrome(options=driver_options)
         case 1:
-            click.echo("Using Firefox...")
-            driver = webdriver.Firefox()
+            click.echo("Driver: Firefox")
+            driver_options = FireFoxOptions()
+            handle_headless(headless)
+            driver = webdriver.Firefox(options=driver_options)
 
     if driver is None:
         click.echo(click.style("You must select a valid driver! use --help to see more information.", fg="red"), err=True, )
         return 
 
     try:
-        print("Process started.")
+        print("Process started. Running...")
         driver.get(URL)
         time.sleep(timeout)
         search_input = driver.find_element(By.ID, "realInput")
@@ -46,10 +60,20 @@ def visit_site(search, browser_id, timeout, headless):
         
         movie_container = driver.find_element(By.CLASS_NAME, "main")
         items = movie_container.find_elements(By.CLASS_NAME, "item")
+        count = 0
         for item in items:
-            click.echo(item.find_element(By.CLASS_NAME, "card-title").text)
+            click.echo("{}. {}".format(click.style(count, fg="green"), item.find_element(By.CLASS_NAME, "card-title").text))
+            count += 1
         
         click.echo("Done!")
+
+        command = questionary.text("Enter movie index: ").ask()
+        item = items[int(command)]
+        click.echo(f"You chose {item.find_element(By.CLASS_NAME, 'card-title').text}")
+        time.sleep(timeout)
+        item.find_element(By.CLASS_NAME, "card-btn").click()
+        time.sleep(timeout)
+        
     except Exception as e:
         click.echo(e, err=True)
     time.sleep(timeout)
@@ -57,7 +81,8 @@ def visit_site(search, browser_id, timeout, headless):
 
 
 def main():
-    visit_site()
+    cli()
+
 if __name__ == "__main__":
     main()
 
