b09502542d39e161da15833d4b909d214f080964
Monday David Osakpawan <rynstheoverlord@gmail.com>
2025-10-15 02:41:09 +0100

    Added search movie feature

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def handle_headless(is_headless: bool):
 @click.command()
 @click.option("-s", "--search", default="", help="Show to search for")
 @click.option("--browser-id", default=0, help="Set browser by ID, 0 for chrome 1 for firefox")
+@click.option("-t", "--timeout", default=3, help="Set length of wait intervals")
 @click.option("-h", "--headless", is_flag=True, help="Use to hide the browser")
-def visit_site(search, browser_id, headless):
+def visit_site(search, browser_id, timeout, headless):
     driver = None
     match browser_id:
         case 0:
@@ -30,19 +31,28 @@ def visit_site(search, browser_id, headless):
             driver = webdriver.Firefox()
 
     if driver is None:
-        click.echo("You must select a valid driver! use --help to see more information.", err=True, )
+        click.echo(click.style("You must select a valid driver! use --help to see more information.", fg="red"), err=True, )
         return 
 
     try:
+        print("Process started.")
         driver.get(URL)
-        time.sleep(2)
+        time.sleep(timeout)
         search_input = driver.find_element(By.ID, "realInput")
         search_input.send_keys(search)
         search_input.send_keys(Keys.ENTER)
+        time.sleep(timeout)
+        click.echo("Result text: {}".format(driver.find_element(By.CLASS_NAME, "search-result-page-h1").text))
+        
+        movie_container = driver.find_element(By.CLASS_NAME, "main")
+        items = movie_container.find_elements(By.CLASS_NAME, "item")
+        for item in items:
+            click.echo(item.find_element(By.CLASS_NAME, "card-title").text)
+        
         click.echo("Done!")
     except Exception as e:
         click.echo(e, err=True)
-    time.sleep(30)
+    time.sleep(timeout)
     print("Program finished!")
 
 
