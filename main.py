6022cebd989181c0b0c6d65bdc2c2c8c53ab8a4b
Monday David Osakpawan <rynstheoverlord@gmail.com>
2025-10-15 14:43:21 +0100

    minor fixes and tool now takes user up to download path, headless now the default state

--- main.py
+++ main.py
@@ -3,45 +3,74 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert 
 import click
 import time, questionary
+import requests 
 
 URL = "https://moviebox.ph/"
 driver_options = None
+
 
 @click.group(help="Download movies from Moviebox to your device")
 def cli():
     pass 
 
-def handle_headless(is_headless: bool): 
+
+def handle_headless(head: bool): 
     if driver_options is None:
         click.echo(click.style("Driver not set!", fg="red"))
         return
-    click.echo(f"headless: {is_headless}")
-    if is_headless:
+    click.echo(f"headless: {not head}")
+    if not head:
         driver_options.add_argument("--headless")
+        #driver_options.add_argument("--window-size=1920,1080")
 
 
+@cli.command(help="Find any potential issues preventing the script from executing successfully")
+def audit():
+    click.echo("Running check...")
+    t0 = time.perf_counter()
+    try:
+        query = requests.get(URL, timeout=6)
+        if query.status_code == 200:
+            click.echo(f"Response: {click.style('SUCCESSFUL(200)', fg='green')}")
+        else:
+            click.echo(f"Response: {click.style(f'BAD({query.status_code})', fg='green')}")
+   
+    except:
+        click.echo(click.style("Request failed terribly! this is most likely a network issue, are you connected to the internet?", fg='red'))
+    t1 = time.perf_counter()
 
+    tn = t1 - t0 
+    click.echo(f"Request speed: {tn}")
 
+    if tn > 6:
+        click.echo(f"Network speed: {click.style('BAD', fg='red')}")
+    elif tn > 3:
+        click.echo(f"Network speed: {click.style('SLOW', fg='yellow')}")
+    else:
+        click.echo(f"Network speed: {click.style('OK', fg='green')}")
+
+    
 @cli.command(help="Find movie to download")
 @click.option("-s", "--search", default="", help="Show to search for")
 @click.option("--browser-id", default=0, help="Set browser by ID, 0 for chrome 1 for firefox")
 @click.option("-t", "--timeout", default=3, help="Set length of wait intervals")
-@click.option("-h", "--headless", is_flag=True, help="Use to hide the browser")
-def find(search, browser_id, timeout, headless):
+@click.option("-h", "--head", is_flag=True, help="Use to Show the browser")
+def find(search, browser_id, timeout, head):
     global driver_options
     driver = None
     match browser_id:
         case 0:
             click.echo("Driver: Chrome")
             driver_options = ChromeOptions()
-            handle_headless(headless)
+            handle_headless(head)
             driver = webdriver.Chrome(options=driver_options)
         case 1:
             click.echo("Driver: Firefox")
             driver_options = FireFoxOptions()
-            handle_headless(headless)
+            handle_headless(head)
             driver = webdriver.Firefox(options=driver_options)
 
     if driver is None:
@@ -73,6 +102,12 @@ def find(search, browser_id, timeout, headless):
         time.sleep(timeout)
         item.find_element(By.CLASS_NAME, "card-btn").click()
         time.sleep(timeout)
+        driver.find_element(By.CLASS_NAME, "watch-btn").click() 
+        time.sleep(timeout)
+        driver.execute_script("""
+document.querySelectorAll('a[href^="xdg-open"], a[href^="intent:"], a[href^="vscode:"], a[href^="zoommtg:"]').forEach(a => a.remove());
+""")
+        driver.find_element(By.CLASS_NAME, "download-btn-hover").click()
         
     except Exception as e:
         click.echo(e, err=True)
